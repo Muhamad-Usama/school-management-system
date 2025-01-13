07286fa1722b74f7b4497813416212c63739dcbe
usama <usama.pakistan90@gmail.com>
2025-01-14 00:33:17 +0500

    add constants

---
+++ src/constants/StatusCodes.js
@@ -0,0 +1,11 @@
+const StatusCodes = {
+    SUCCESS: 200,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 1004, // Custom code for "not found"
+    INTERNAL_SERVER_ERROR: 500,
+    // Add more codes as needed
+};
+
+module.exports = StatusCodes;
