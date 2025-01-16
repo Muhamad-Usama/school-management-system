bde9f9fe03e7935f2c09da8c916a7426d67bc53a
usama <03068021348usa@gmail.com>
2025-01-17 04:14:59 +0500

    refactor

--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ const {INTERNAL_SERVER_ERROR} = require("./constants/StatusCodes");
 const BaseResponse = require("./base/BaseResponse");
 const {connectMongo} = require("./config/mongo");
 const {connectRedis} = require("./config/redisClient");
+const {translate} = require("./config/i18n");
 const PORT = process.env.PORT || 8000;
 
 app.use(async (err, req, res, next) => {
     const errorCode = err.status ?? INTERNAL_SERVER_ERROR;
-    const errorMessage = await req.t(err.message.trim() ?? "something_went_wrong");
+    const errorMessage = err.message.trim() ?? "something.went.wrong";
     return res.status(200).json(BaseResponse.error(errorCode, errorMessage));
 });
 
