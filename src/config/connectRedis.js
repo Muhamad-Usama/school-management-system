fbcef347080b7a911d9b7ae0d355e451effeeb7d
usama <03068021348usa@gmail.com>
2025-01-17 03:57:07 +0500

    add redis config

---
+++ src/config/connectRedis.js
@@ -0,0 +1,21 @@
+const {createClient} = require("redis");
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
+
+redisClient.on("error", (err) => console.error("redis.client.error", err));
+
+const connectRedis = async () => {
+    try {
+        if (!redisClient.isOpen) {
+            await redisClient.connect();
+        }
+        console.log("redis.client.connected");
+    } catch (error) {
+        console.error("redis.client.failed", error);
+        throw error;
+    }
+};
+
+module.exports = {redisClient, connectRedis};
