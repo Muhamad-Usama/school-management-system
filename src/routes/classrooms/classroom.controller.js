3d90652f4e5255a97f6b7816a8700ba1ceda2f97
usama <03068021348usa@gmail.com>
2025-01-20 03:19:36 +0500

    add final changes

--- src/routes/classrooms/classroom.controller.js
+++ src/routes/classrooms/classroom.controller.js
@@ -1,11 +1,21 @@
 const {
-    createClassroom, getClassrooms, getClassroomById, updateClassroom, deleteClassroom
+    findClassroomById,
+    saveClassroom,
+    updateClassroomById,
+    deleteClassroomById,
+    getAllClassrooms,
+    existsClassroomByName
 } = require('../../models/classrooms/classroom.model');
 const BaseResponse = require("../../base/BaseResponse");
+const StatusCodes = require("../../constants/StatusCodes");
 
 const httpCreateClassroom = async (req, res) => {
     try {
-        const classroom = await createClassroom(req.body);
+        const exists = await existsClassroomByName(req.body.name);
+        if(exists) {
+            return res.status(409).json(BaseResponse.error(StatusCodes.DUPLICATE_RECORD, 'classrooms.already.exists'));
+        }
+        const classroom = await saveClassroom(req.body);
         res.status(201).json(BaseResponse.success(classroom));
     } catch (error) {
         res.status(500).json({error: error.message});
@@ -14,7 +24,7 @@ const httpCreateClassroom = async (req, res) => {
 
 const httpGetClassrooms = async (req, res) => {
     try {
-        const classrooms = await getClassrooms();
+        const classrooms = await getAllClassrooms();
         res.status(200).json(BaseResponse.success(classrooms));
     } catch (error) {
         res.status(500).json({error: error.message});
@@ -23,9 +33,9 @@ const httpGetClassrooms = async (req, res) => {
 
 const httpGetClassroomById = async (req, res) => {
     try {
-        const classroom = await getClassroomById(req.params.id);
+        const classroom = await findClassroomById(req.params.id);
         if (!classroom) {
-            return res.status(404).json(BaseResponse.notFoundError('classrooms.not.found'));
+            return res.status(404).json(BaseResponse.notFoundError('classroom.not.found'));
         }
         res.status(200).json(BaseResponse.success(classroom));
     } catch (error) {
@@ -35,9 +45,9 @@ const httpGetClassroomById = async (req, res) => {
 
 const httpUpdateClassroom = async (req, res) => {
     try {
-        const classroom = await updateClassroom(req.params.id, req.body);
+        const classroom = await updateClassroomById(req.params.id, req.body);
         if (!classroom) {
-            return res.status(404).json(BaseResponse.notFoundError('classrooms.not.found'));
+            return res.status(404).json(BaseResponse.notFoundError('classroom.not.found'));
         }
         res.status(200).json(BaseResponse.success(classroom));
     } catch (error) {
@@ -47,9 +57,9 @@ const httpUpdateClassroom = async (req, res) => {
 
 const httpDeleteClassroom = async (req, res) => {
     try {
-        const classroom = await deleteClassroom(req.params.id);
+        const classroom = await deleteClassroomById(req.params.id);
         if (!classroom) {
-            return res.status(404).json(BaseResponse.notFoundError('classrooms.not.found'));
+            return res.status(404).json(BaseResponse.notFoundError('classroom.not.found'));
         }
         res.status(200).json(BaseResponse.success({deleted: true}));
     } catch (error) {
