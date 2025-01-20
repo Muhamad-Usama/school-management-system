4e4d9a2de7520c7054674c3b49c444f04dec594e
usama <03068021348usa@gmail.com>
2025-01-21 03:13:35 +0500

    update connection string

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // server setup
-const MONGO_URL = process.env.MONGO_URI;
+const MONGO_URL = process.env.MONGO_URI ?? "mongodb://localhost:27017/school_management_system";
 
 // use once if you are sure that event will be emitted once
 
@@ -14,9 +14,7 @@ mongoose.connection.on("error", (err) => {
 });
 
 async function connectMongo() {
-    await mongoose.connect(MONGO_URL?.toString(), {
-        useNewUrlParser: true, useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGO_URL);
 }
 
 module.exports = {
